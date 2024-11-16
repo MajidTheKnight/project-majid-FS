039402c03b3fadcb3eb95c8de81742e817bfd24b
majid <majidgamerknight@gmail.com>
2024-11-16 22:06:21 +0330

    second

---
+++ about.php
@@ -0,0 +1,7 @@
+<?php
+include("head.html")
+?>
+<p>به زودی اضافه خواهد شد</p>
+<?php
+include("foot.html")
+?>
---
+++ contact.php
@@ -0,0 +1,8 @@
+<?php
+include("head.html")
+?>
+<p>تلفن پشتیبانی:۰۹۱۳۷۳۰۵۲۳۱</p>
+<p>ایدی تلگرام:MajidTheKnight</p>
+<?php
+include("foot.html")
+?>
