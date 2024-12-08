693d6a34b40223649d818cf0491485b32110a5cf
majid <majidgamerknight@gmail.com>
2024-12-08 13:43:21 +0330

    5th commit

--- index.php
+++ index.php
@@ -1,7 +1,22 @@
 <?php
 include("head.html")
 ?>
-<p>خوش امدید.برای دسترسی به صفحات مختلف سایت از لینک های بالا استفاده کنید</p>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="project.css" class="rel">
+</head>
+<body>
+    <section class="home">
+        <div class="home-content"></div>    
+    <p id="welcome" >خوش امدید.برای دسترسی به صفحات مختلف سایت از لینک های بالا استفاده کنید</p>
+    <script> src="mw.jpg"</script>
+    </section>
+</body>
+</html>
  <?php
 include("foot.html")
 ?>
