a2da5e8eb03e5187bfe369038d91d9990aff4d99
majid <majidgamerknight@gmail.com>
2024-12-15 12:59:36 +0330

    7th commit

--- project.php
+++ project.php
@@ -1,10 +1,23 @@
 <?php
 include("head.html");
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="project.css">
+    <link rel="stylesheet" href="bootstrap.css">
+</head>
+
+<body>
+    
+
 <main class="container mt-5">
     <div>
         <div>
-            <p class="display-4 text-center">سبک بازی های محبوب جهان</p>
+            <p class="display-4 text-center ">سبک بازی های محبوب جهان</p>
         </div>
         <div>
             <p class="lead text-justify">امروزه بازی‌ های ویدیویی زیادی در سبک‌های مختلف طراحی شده و وارد بازار می‌شوند. شرکت‌ها و استودیوهای بازی، همیشه تمام تلاش خود را دارند تا بهترین اثر را روانه بازار کنند. برخی از شرکت‌ها تنها به تولید آثار انحصاری خود در یک سبک خاص می‌پردازند و عده‌ای دیگر سعی می‌کنند تا بازی‌ها را در ژانرهای مختلف و مطابق با تمام سلایق تولید کنند. دسته‌بندی بازی‌ها براساس گیم پلی آنها انجام می‌شود و به اصل داستان و موارد دیگر ارتباطی ندارد. اگر دوست دارید با انواع بازی‌های دیجیتال و رایانه‌ای آشنا شوید و ببینید کدامیک با سلیقه‌تان سازگارتر هست، این مقاله را تا انتها دنبال کنید و با دنیای وسیع بازی‌ها بیشتر آشنا شوید.</p>
@@ -21,40 +34,69 @@ include("head.html");
 
         <div class="my-5">
             <p class="display-4 text-center">بازیهای اکشن</p>
-            <img class="img-fluid" src="mortalkombat.jpg" alt="Mortal Kombat">
+            <div class="row justify-content-center">
+                <div class="col-md-6 col-12">
+                    <img class="img-fluid rounded shadow" src="mortalkombat.jpg" alt="Mortal Kombat">
+                </div>
+            </div>
             <p class="lead text-justify mt-3">بازی اکشن به بازی‌ای گفته می‌شود که نیاز به فعالیت فیزیکی شخصیت اصلی دارد و شامل زدوخوردهای زیاد و جست و خیز می‌باشد. برای پیشروی در این بازی‌ها‌، نیاز به دویدن، رفتن به اماکن مختلف، مبارزه با دشمن و حفظ نوار سلامت دارید. بیشتر بازی‌های ژانر اکشن دارای نوار سلامتی برای بازیکن هستند. هرچه بتوان در مبارزات جان سالم به در برد و نوار سلامت خود را بیشتر حفظ کرد، به امتیاز بالاتر و پیشروی در بازی دست پیدا خواهید کرد. این بازی شامل زیرسبک‌هایی از بازی‌های مبارزه‌ا‌ی، بزن بزن و نبرد چند نفره می‌باشد. بازی‌های Fortnite، Counter Strike Global offensive، Mortal Kombat، Halo، Call of Duty و Doom Eternal از جمله‌ بازی‌های محبوب ژانر اکشن هستند.</p>
         </div>
 
         <div class="my-5">
             <p class="display-4 text-center">بازی‌ های شوتر</p>
-            <img class="img-fluid" src="Cod1.jpg" alt="Call of Duty">
+            <div class="row justify-content-center">
+                <div class="col-md-6 col-12">
+                    <img class="img-fluid rounded shadow" src="Cod1.jpg" alt="Call of Duty">
+                </div>
+            </div>
             <p class="lead text-justify mt-3">بازی شوتر یا همان تیراندازی، یکی از بازی‌های پرطرفدار میان بازیکنان است که نیاز به سرعت واکنش بالا و دقت در تیراندازی دارد. این بازی‌ها به صورت تک نفره و گروهی قابل اجرا هستند؛ در بازی گروهی باید تمام بازیکنان عکس‌العمل سریع داشته باشند تا بتوانند به پیروی دست پیدا کنند. این ژانر بازی شامل زیرسبک‌های تیراندازی اول شخص، تیراندازی سوم شخص، تیراندازی تاکتیکی و شوتم آپ می‌باشد. از بازی‌های معروف شوتر می‌توان Call of Duty، Metro 2033، Far Cry، Halo و Tom Clancy’s Rainbow Six Seige را نام برد.</p>
         </div>
 
         <div class="my-5">
             <p class="display-4 text-center">بازی های ماجراجویی</p>
-            <img class="img-fluid" src="AC.jpg" alt="Assassin's Creed">
+            <div class="row justify-content-center">
+                <div class="col-md-6 col-12">
+                    <img class="img-fluid rounded shadow" src="AC.jpg" alt="Assassin's Creed">
+                </div>
+            </div>
             <p class="lead text-justify mt-3">بازی‌ ژانر ماجراجویی اثری است که بازیکن باید به کاوش و جستجو در محیط بازی بپردازد و با پیدا کردن سرنخ، چالش‌ها را پشت سر بگذارد. این نوع بازی شامل زیرسبک‌های ماجراجویی گرافیکی، متنی و چند مورد دیگر می‌باشد. از معروفترین بازی‌های سبک ماجراجویی می‌توان به Peaky Blinders Mastermind، Tell Me why و What Remains of Edith Finch اشاره کرد.</p>
         </div>
 
         <div class="my-5">
             <p class="display-4 text-center">بازی های ورزشی</p>
-            <img class="img-fluid" src="PES.jpg" alt="Pro Evolution Soccer">
+            <div class="row justify-content-center">
+                <div class="col-md-6 col-12">
+                    <img class="img-fluid rounded shadow" src="PES.jpg" alt="Pro Evolution Soccer">
+                </div>
+            </div>
             <p class="lead text-justify mt-3">بازی‌های ورزشی همانطور که از نامشان پیداست، به درگیری بازیکن با یک ورزش خاص مانند فوتبال و بسکتبال اشاره دارد. این ژانر بازی دارای زیرسبک‌های مسابقه‌ای و شبیه‌سازی ورزشها می‌باشد. بازیکن با خرید این نوع بازی‌ها می‌تواند خود را در قالب یک شخصیت محبوب ورزشی قرار دهد و با آن به اجرای بازی بپردازد. از معروفترین بازی‌های ژانر ورزشی می‌توان به سری NBA 2K21، سری فیفا، Tennis World Tour و Captain TSBASA: Rise Of New Champions اشاره کرد.</p>
         </div>
 
         <div class="my-5">
             <p class="display-4 text-center">بازی های نقش افرینی</p>
-            <img class="img-fluid" src="demonsSouls.jpg" alt="Demon's Souls">
+            <div class="row justify-content-center">
+                <div class="col-md-6 col-12">
+                    <img class="img-fluid rounded shadow" src="demonsSouls.jpg" alt="Demon's Souls">
+                </div>
+            </div>
             <p class="lead text-justify mt-3">بازی نقش‌آفرینی ژانری از بازی‌های ویدئویی است که بازیکن در آن هدایت یک یا چندین قهرمان را برعهده دارد. گیمر در این بازی درگیر داستانی پیچیده شده و طی پیشروی در آن، به پیشرفت شخصیتی می‌رسد. این سبک بازی شامل زیرسبک‌هایی همچون نقش‌آفرینی اکشن، تاکتیکی و سندباکس می‌شود. از محبوبترین آثار نقش‌آفرینی می‌توان بازی‌های Demon’s Souls Remake، The Witcher 3 Wild Hunt، Disco Elysium، Kingdom Come Deliverance را نام برد.</p>
         </div>
 
         <div class="my-5">
             <p class="display-4 text-center">بازی های استراتژی</p>
-            <img class="img-fluid" src="StrongholdLegends.jpg" alt="Stronghold Legends">
+            <div class="row justify-content-center">
+                <div class="col-md-6 col-12">
+                    <img class="img-fluid rounded shadow" src="StrongholdLegends.jpg" alt="Stronghold Legends">
+                </div>
+            </div>
             <p class="lead text-justify mt-3">بازی‌های ژانر استراتژی نیاز به انتخاب یک استراتژی مناسب دارند تا بتوان به هدف اصلی و پیروزی رسید. با انتخاب بهترین راه می‌توان به راحتی بر حریف غلبه کرد و بازی را پیش برد. بازی‌های استراتژی نوبتی و استراتژی همزمان از انواع زیرسبک این ژانر به حساب می‌آیند. از بازی‌های این ژانر می‌توانیم به Total War Sega: Troy، Warcraft III و Gears Tactics اشاره کنیم.</p>
         </div>
-</main>    
+</main>  
+
+
+</body>
+
+</html>
 
 <?php
 include("foot.html");
